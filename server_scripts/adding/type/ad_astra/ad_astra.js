7e05e7009e42a07bb7df2ddefe1df977e6f7cf10
LorpOOO <150320219+LorpOOO@users.noreply.github.com>
2026-06-17 23:21:59 +0300

    Update ad_astra.js

--- server_scripts/adding/type/ad_astra/ad_astra.js
+++ server_scripts/adding/type/ad_astra/ad_astra.js
@@ -5,12 +5,12 @@ ServerEvents.recipes(event => {
         ingredients: [
             { item: "roll_mod:nose_mk1" },
 
-            { item: "modern_industrialization:space_alloy_machine_casing_pipe" },
-            { item: "modern_industrialization:space_alloy_machine_casing_pipe" },
-            { item: "modern_industrialization:space_alloy_machine_casing_pipe" },
-            { item: "modern_industrialization:space_alloy_machine_casing_pipe" },
-            { item: "modern_industrialization:space_alloy_machine_casing_pipe" },
-            { item: "modern_industrialization:space_alloy_machine_casing_pipe" },
+            { item: "modern_industrialization:cosmic_alloy_two_machine_casing_pipe" },
+            { item: "modern_industrialization:cosmic_alloy_two_machine_casing_pipe" },
+            { item: "modern_industrialization:cosmic_alloy_two_machine_casing_pipe" },
+            { item: "modern_industrialization:cosmic_alloy_two_machine_casing_pipe" },
+            { item: "modern_industrialization:cosmic_alloy_two_machine_casing_pipe" },
+            { item: "modern_industrialization:cosmic_alloy_two_machine_casing_pipe" },
 
             { item: "roll_mod:fin_mk1" },
 
@@ -27,6 +27,36 @@ ServerEvents.recipes(event => {
         result: {
             count: 1,
             id: "ad_astra:tier_1_rocket"
+        }
+    })
+
+        event.custom({
+        type: "ad_astra:nasa_workbench",
+        ingredients: [
+            { item: "roll_mod:nose_mk2" },
+
+            { item: "modern_industrialization:cosmic_alloy_two_machine_casing_pipe" },
+            { item: "modern_industrialization:cosmic_alloy_two_machine_casing_pipe" },
+            { item: "modern_industrialization:cosmic_alloy_two_machine_casing_pipe" },
+            { item: "modern_industrialization:cosmic_alloy_two_machine_casing_pipe" },
+            { item: "modern_industrialization:cosmic_alloy_two_machine_casing_pipe" },
+            { item: "modern_industrialization:cosmic_alloy_two_machine_casing_pipe" },
+
+            { item: "roll_mod:fin_mk2" },
+
+            { item: "roll_mod:tank_mk2" },
+            { item: "roll_mod:tank_mk2" },
+
+            { item: "roll_mod:fin_mk2" },
+            { item: "roll_mod:fin_mk2" },
+
+            { item: "roll_mod:engine_mk2" },
+
+            { item: "roll_mod:fin_mk2" }
+        ],
+        result: {
+            count: 1,
+            id: "ad_astra:tier_2_rocket"
         }
     })
 })
