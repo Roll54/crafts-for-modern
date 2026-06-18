eb247d86371ae686e356ba00e531cda30f810b01
LorpOOO <150320219+LorpOOO@users.noreply.github.com>
2026-06-18 15:36:42 +0300

    Update canning_machine.js

--- server_scripts/adding/type/modern_industrialization/canning_machine.js
+++ server_scripts/adding/type/modern_industrialization/canning_machine.js
@@ -4,8 +4,8 @@ event.recipes.extended_industrialization.canning_machine(4, 200)
 .itemIn("1x roll_mod:empty_primitive_battery")
 .itemOut("1x roll_mod:primitive_battery")
 
-.fluidIn("20000x modern_industrialization:rocket_propellant")
-.itemIn("1x roll_mod:fuel_canister")
-.itemOut("1x roll_mod:rocket_fuel_canister")
+//.fluidIn("20000x modern_industrialization:rocket_propellant")
+//.itemIn("1x roll_mod:fuel_canister")
+//.itemOut("1x roll_mod:rocket_fuel_canister")
 
 })
