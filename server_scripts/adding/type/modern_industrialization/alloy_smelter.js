de0392f8e8c38d2a4fe83728a56b670d2048e50b
LorpOOO <150320219+LorpOOO@users.noreply.github.com>
2026-06-19 23:22:37 +0300

    Update alloy_smelter.js

--- server_scripts/adding/type/modern_industrialization/alloy_smelter.js
+++ server_scripts/adding/type/modern_industrialization/alloy_smelter.js
@@ -1,15 +1,15 @@
 ServerEvents.recipes(event => {
 
 event.recipes.extended_industrialization.electric_alloy_smelter(4, 400)
-  .itemIn("1x minecraft:copper_ingot")
-  .itemIn("1x modern_industrialization:redstone_alloy_ingot")
-  .itemOut("8x ad_astra:calorite_ingot");
-
-event.recipes.extended_industrialization.electric_alloy_smelter(4, 400)
   .itemIn("roll_mod:bismuth_dust")
   .itemIn("minecraft:copper_ingot")
   .itemOut("8x ad_astra:ostrum_ingot")
 
+event.recipes.extended_industrialization.electric_alloy_smelter(4, 400)
+  .itemIn("1x minecraft:copper_ingot")
+  .itemIn("1x modern_industrialization:redstone_alloy_ingot")
+  .itemOut("8x ad_astra:calorite_ingot")
+  
 event.recipes.extended_industrialization.electric_alloy_smelter(4, 400)
   .itemIn("minecraft:gold_ingot")
   .itemIn("modern_industrialization:redstone_alloy_ingot")
