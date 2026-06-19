18dd2ec56ef7fb4ff7271830178376c6efa8d8f5
LorpOOO <150320219+LorpOOO@users.noreply.github.com>
2026-06-19 18:44:49 +0300

    Update ad_astra.js

--- server_scripts/delete/ad_astra.js
+++ server_scripts/delete/ad_astra.js
@@ -1,5 +1,4 @@
 ServerEvents.recipes(event => {
-  // Видалення ракет Ad Astra (як у твоєму прикладі)
-  event.remove({ output: "ad_astra:tier_1_rocket" })
-  event.remove({ output: "ad_astra:tier_2_rocket" })
+  event.remove({ id: 'ad_astra:nasa_workbench/tier_1_rocket_from_nasa_workbench' })
+  event.remove({ id: 'ad_astra:nasa_workbench/tier_2_rocket_from_nasa_workbench' })
 })
