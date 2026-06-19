91cd7c2ddda2faa902e88cd8b1e6493548ddd2e3
LorpOOO <150320219+LorpOOO@users.noreply.github.com>
2026-06-19 20:48:23 +0300

    Update compressor.js

--- server_scripts/adding/type/modern_industrialization/compressor.js
+++ server_scripts/adding/type/modern_industrialization/compressor.js
@@ -8,6 +8,18 @@ ServerEvents.recipes(event => {
   .itemIn("5x modern_industrialization:diamond_dust")
   .itemOut("2x modern_industrialization:diamond_plate")
 
+  event.recipes.modern_industrialization.compressor(2, 400)
+    .itemIn("ad_astra:ostrum_ingot")
+    .itemOut("ad_astra:ostrum_plate")
+
+  event.recipes.modern_industrialization.compressor(2, 400)
+    .itemIn("ad_astra:desh_ingot")
+    .itemOut("ad_astra:desh_plate")
+
+  event.recipes.modern_industrialization.compressor(2, 400)
+    .itemIn("ad_astra:calorite_ingot")
+    .itemOut("ad_astra:calorite_plate")
+
   /* Tech Reborn
   event.recipes.modern_industrialization.compressor(32, 800)
   .itemIn("8x kubejs:energium_dust")
