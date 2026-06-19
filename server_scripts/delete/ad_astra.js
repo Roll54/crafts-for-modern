fa712dcdc8b1741feb6a4a83916e9bffe578cdd7
LorpOOO <150320219+LorpOOO@users.noreply.github.com>
2026-06-19 16:46:08 +0300

    Update ad_astra.js

--- server_scripts/delete/ad_astra.js
+++ server_scripts/delete/ad_astra.js
@@ -1,3 +1,5 @@
 ServerEvents.recipes(event => {
-  event.remove({ mod: 'ad_astra' })
+  // Видалення крафтів ракет Tier 1 та Tier 2
+  event.remove({ output: 'ad_astra:tier_1_rocket' })
+  event.remove({ output: 'ad_astra:tier_2_rocket' })
 })
