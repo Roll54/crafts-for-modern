6e5cf3f2bb1098c27998a712bb3b8a2c7a7b7132
Roll54 <roll4248666@gmail.com>
2026-06-19 16:40:19 -0400

    try 4

--- server_scripts/adding/type/modern_industrialization/alloy_smelter.js
+++ server_scripts/adding/type/modern_industrialization/alloy_smelter.js
@@ -14,12 +14,12 @@ event.recipes.extended_industrialization.electric_alloy_smelter(4, 400)
   .itemIn("1x minecraft:gold_ingot")
   .itemIn("1x modern_industrialization:redstone_alloy_ingot")
   .itemOut("8x ad_astra:desh_ingot")
-
+*/
 event.recipes.extended_industrialization.alloy_smelter(4, 400)
 .itemIn("3x roll_mod:raw_rubber")
 .itemIn("1x #c:dusts/sulfur")
 .itemOut("1x roll_mod:rubber_ingot")
-*/
+
 event.recipes.extended_industrialization.alloy_smelter(4, 400)
 .itemIn("5x #c:dusts/copper")
 .itemIn("4x #c:dusts/electrum")
