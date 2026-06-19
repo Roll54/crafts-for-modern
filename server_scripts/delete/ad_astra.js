e99062e3f0a1b85eb1bbcfde1e8a6b92363ba9ab
LorpOOO <150320219+LorpOOO@users.noreply.github.com>
2026-06-19 16:59:21 +0300

    Update ad_astra.js

--- server_scripts/delete/ad_astra.js
+++ server_scripts/delete/ad_astra.js
@@ -1,13 +1,5 @@
-// kubejs/server_scripts/remove_rockets.js
-
 ServerEvents.recipes(event => {
-  // Прибираємо самі предмети з гри
-  event.remove({ id: 'ad_astra:tier_1_rocket' })
-  event.remove({ id: 'ad_astra:tier_2_rocket' })
-})
-
-// Ховаємо їх в JEI
-JEIEvents.hideItems(event => {
-  event.hide('ad_astra:tier_1_rocket')
-  event.hide('ad_astra:tier_2_rocket')
+  // Видалення ракет Ad Astra (як у твоєму прикладі)
+  event.remove({ output: "ad_astra:tier_1_rocket" })
+  event.remove({ output: "ad_astra:tier_2_rocket" })
 })
