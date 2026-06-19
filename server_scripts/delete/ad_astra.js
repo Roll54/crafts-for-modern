b84e8f624d7a9ba93babd31454e88124d5a189a6
LorpOOO <150320219+LorpOOO@users.noreply.github.com>
2026-06-19 16:35:28 +0300

    Update ad_astra.js

--- server_scripts/delete/ad_astra.js
+++ server_scripts/delete/ad_astra.js
@@ -1,9 +1,5 @@
 ServerEvents.recipes(event => {
-  event.remove({output: 'ad_astra:tier_1_rocket'})
-  event.remove({output: 'ad_astra:tier_2_rocket'})
-})
 
-JEIEvents.hideItems(event => {
-  event.hide('ad_astra:tier_1_rocket')
-  event.hide('ad_astra:tier_2_rocket')
+event.remove({ output: "ad_astra:tier_1_rocket" })
+event.remove({ output: "ad_astra:tier_2_rocket" })
 })
