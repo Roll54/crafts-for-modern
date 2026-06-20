812d74b9598558e55dbdf274000d54e3dbc48ef3
Roll54 <roll4248666@gmail.com>
2026-06-20 00:27:56 -0400

    иглмш

--- server_scripts/adding/type/farmersdelight/cutting.js
+++ server_scripts/adding/type/farmersdelight/cutting.js
@@ -1,13 +1,22 @@
 ServerEvents.recipes(event => {
-    event.custom({
-        type: 'farmersdelight:cutting',
-        ingredients: [
-            { tag: 'createcybernetics:offal' }
-        ],
-        result: [
-            { item: 'createcybernetics:ground_offal', count: 1 }
-        ],
-        sound: 'farmersdelight:block.cutting_board.knife',
-        tool: { tag: 'c:tools/knife' }
-    })
+
+event.custom({
+    type: 'farmersdelight:cutting',
+    ingredients: [
+        { tag: 'createcybernetics:offal' }
+    ],
+    result: [
+        {
+            item: {
+                id: 'createcybernetics:ground_offal',
+                count: 1
+            }
+        }
+    ],
+    sound: 'farmersdelight:block.cutting_board.knife',
+    tool: {
+        tag: 'c:tools/knife'
+    }
+})
+
 })
