3cc24f0d0ed1db54951913ec7e2c62e36e399955
LorpOOO <150320219+LorpOOO@users.noreply.github.com>
2026-06-20 06:38:11 +0300

    Delete server_scripts/adding/type/smoker.js

--- server_scripts/adding/type/smoker.js
+++
@@ -1,8 +0,0 @@
-ServerEvents.recipes(event => {
-    event.smoking('createcybernetics:cooked_bodypart_brain', 'createcybernetics:bodypart_brain').xp(0.35)
-    event.smoking('createcybernetics:cooked_bodypart_heart', 'createcybernetics:bodypart_heart').xp(0.35)
-    event.smoking('createcybernetics:cooked_bodypart_liver', 'createcybernetics:bodypart_liver').xp(0.35)
-    event.smoking('createcybernetics:cooked_andouille_sausage', 'createcybernetics:andouille_sausage').xp(0.35)
-    event.smoking('createcybernetics:cooked_ground_offal', 'createcybernetics:ground_offal').xp(0.35)
-    event.smoking('createcybernetics:bone_marrow', 'minecraft:bone').xp(0.35)
-})
