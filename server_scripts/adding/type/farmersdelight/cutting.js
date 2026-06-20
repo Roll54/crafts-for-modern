5f10f3e3a8072bfddae7a6550bf96f415d52b9fd
LorpOOO <150320219+LorpOOO@users.noreply.github.com>
2026-06-20 06:37:55 +0300

    Create cutting.js

---
+++ server_scripts/adding/type/farmersdelight/cutting.js
@@ -0,0 +1,14 @@
+ServerEvents.recipes(event => {
+
+    event.custom({
+        type: 'farmersdelight:cutting',
+        ingredients: [
+            { tag: 'createcybernetics:offal' }
+        ],
+        result: [
+            { item: 'createcybernetics:ground_offal', count: 1 }
+        ],
+        sound: 'farmersdelight:block.cutting_board.knife',
+        tool: { tag: 'c:tools/knives' }
+    })
+})
