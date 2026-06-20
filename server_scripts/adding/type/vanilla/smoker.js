9e4e0c628ea1b84de8f049f405391069c0f446a2
LorpOOO <150320219+LorpOOO@users.noreply.github.com>
2026-06-20 07:17:05 +0300

    Update smoker.js

--- server_scripts/adding/type/vanilla/smoker.js
+++ server_scripts/adding/type/vanilla/smoker.js
@@ -1,8 +1,8 @@
 ServerEvents.recipes(event => {
-    event.smoking('createcybernetics:cooked_bodypart_brain', 'createcybernetics:bodypart_brain').xp(0.35)
-    event.smoking('createcybernetics:cooked_bodypart_heart', 'createcybernetics:bodypart_heart').xp(0.35)
-    event.smoking('createcybernetics:cooked_bodypart_liver', 'createcybernetics:bodypart_liver').xp(0.35)
-    event.smoking('createcybernetics:cooked_andouille_sausage', 'createcybernetics:andouille_sausage').xp(0.35)
+    event.smoking('createcybernetics:cooked_brain', 'createcybernetics:bodypart_brain').xp(0.35)
+    event.smoking('createcybernetics:cooked_heart', 'createcybernetics:bodypart_heart').xp(0.35)
+    event.smoking('createcybernetics:cooked_liver', 'createcybernetics:bodypart_liver').xp(0.35)
+    event.smoking('createcybernetics:roasted_andouille', 'createcybernetics:andouille_sausage').xp(0.35)
     event.smoking('createcybernetics:cooked_ground_offal', 'createcybernetics:ground_offal').xp(0.35)
     event.smoking('createcybernetics:bone_marrow', 'minecraft:bone').xp(0.35)
 })
