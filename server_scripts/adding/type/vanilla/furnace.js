a410ca41bce881bd695fc84b972492e3046afea7
LorpOOO <150320219+LorpOOO@users.noreply.github.com>
2026-06-22 20:51:29 +0300

    Update furnace.js

--- server_scripts/adding/type/vanilla/furnace.js
+++ server_scripts/adding/type/vanilla/furnace.js
@@ -13,6 +13,7 @@ event.smelting('2x minecraft:lapis_lazuli', 'roll_mod:raw_lapis_lazuli')
 event.smelting('createcybernetics:cooked_brain', 'createcybernetics:bodypart_brain').xp(0.35)
 event.smelting('createcybernetics:cooked_heart', 'createcybernetics:bodypart_heart').xp(0.35)
 event.smelting('createcybernetics:cooked_liver', 'createcybernetics:bodypart_liver').xp(0.35)
+event.smelting('minecraft:brick', '4x roll_mod:clay_dust').xp(0.35)
 event.smelting('createcybernetics:roasted_andouille', 'createcybernetics:andouille_sausage').xp(0.35)
 event.smelting('createcybernetics:bone_marrow', 'minecraft:bone').xp(0.35)
 
