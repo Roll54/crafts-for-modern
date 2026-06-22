4deb14a18fa39f760b18f6730b24ac46183ea521
LorpOOO <150320219+LorpOOO@users.noreply.github.com>
2026-06-22 09:52:45 +0300

    Update upgrades.js

--- server_scripts/delete/sopphistictedbackpacks/upgrades.js
+++ server_scripts/delete/sopphistictedbackpacks/upgrades.js
@@ -1,5 +1,7 @@
 ServerEvents.recipes(event => {
 
+        event.remove({ output: "sophisticatedbackpacks:mob_catcher_upgrade" })
+        event.remove({ output: "sophisticatedbackpacks:advanced_mob_catcher_upgrade" })
         event.remove({ output: "sophisticatedbackpacks:inception_upgrade" })
         event.remove({ output: "sophisticatedbackpacks:feeding_upgrade" })
         event.remove({ output: "sophisticatedbackpacks:advanced_feeding_upgrade" })
