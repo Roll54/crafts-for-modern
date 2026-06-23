ceeb70f942b1cb8c6d7c01aaecebe9c3245104e0
LorpOOO <150320219+LorpOOO@users.noreply.github.com>
2026-06-23 07:57:02 +0300

    Update convert.js

--- server_scripts/adding/type/silentgear/convert.js
+++ server_scripts/adding/type/silentgear/convert.js
@@ -1,50 +1,42 @@
 // server_scripts/adding/type/silentgear/convert.js
 
 ServerEvents.recipes(event => {
-  const tools = [
-    { input: 'roll_mod:meteorite_sword', output: 'silentgear:sword', head: 'sword_blade' },
-    { input: 'roll_mod:meteorite_pickaxe', output: 'silentgear:pickaxe', head: 'pickaxe_head' },
-    { input: 'roll_mod:meteorite_axe', output: 'silentgear:axe', head: 'axe_head' },
-    { input: 'roll_mod:meteorite_shovel', output: 'silentgear:shovel', head: 'shovel_head' },
-    { input: 'roll_mod:meteorite_hoe', output: 'silentgear:hoe', head: 'hoe_head' }
-  ]
-
-  tools.forEach(tool => {
-    event.custom({
-      type: 'silentgear:conversion',
-      category: 'misc',
-      ingredients: [{ item: tool.input }],
-      result: {
-        id: tool.output,
-        count: 1,
-        parts: [  // <-- Це МАСИВ!
-          {
-            item: {
-              id: `silentgear:${tool.head}`,
-              count: 1,
-              components: {
-                'silentgear:material_list': [
-                  { material: 'silentgear:meteorite_metal' },
-                  { material: 'silentgear:meteorite_metal' }
-                ]
-              }
-            },
-            part: `silentgear:${tool.head}`
+  event.custom({
+    type: 'silentgear:conversion',
+    category: 'misc',
+    ingredients: [
+      { item: 'roll_mod:meteorite_sword' }
+    ],
+    result: {
+      id: 'silentgear:sword',
+      count: 1,
+      parts: [
+        {
+          item: {
+            id: 'silentgear:sword_blade',
+            count: 1,
+            components: {
+              'silentgear:material_list': [
+                { material: 'silentgear:meteorite_metal' },
+                { material: 'silentgear:meteorite_metal' }
+              ]
+            }
           },
-          {
-            item: {
-              id: 'silentgear:rod',
-              count: 1,
-              components: {
-                'silentgear:material_list': [
-                  { material: 'silentgear:wood' }
-                ]
-              }
-            },
-            part: 'silentgear:rod'
-          }
-        ]  // <-- Закриття масиву
-      }
-    })
+          part: 'silentgear:sword_blade'
+        },
+        {
+          item: {
+            id: 'silentgear:rod',
+            count: 1,
+            components: {
+              'silentgear:material_list': [
+                { material: 'silentgear:wood' }
+              ]
+            }
+          },
+          part: 'silentgear:rod'
+        }
+      ]
+    }
   })
 })
