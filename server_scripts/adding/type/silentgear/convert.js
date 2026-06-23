0706559dc1316a683f3280ab11089ff3dca24117
LorpOOO <150320219+LorpOOO@users.noreply.github.com>
2026-06-23 08:05:34 +0300

    Fully functional convert.js

--- server_scripts/adding/type/silentgear/convert.js
+++ server_scripts/adding/type/silentgear/convert.js
@@ -1,14 +1,8 @@
-// server_scripts/adding/type/silentgear/convert.js
-
 ServerEvents.recipes(event => {
   event.custom({
     "type": "silentgear:conversion",
     "category": "misc",
-    "ingredients": [
-      {
-        "item": "roll_mod:meteorite_sword"
-      }
-    ],
+    "ingredients": [{ "item": "roll_mod:meteorite_sword" }],
     "result": {
       "item": "silentgear:sword",
       "parts": [
@@ -16,12 +10,8 @@ ServerEvents.recipes(event => {
           "item": {
             "components": {
               "silentgear:material_list": [
-                {
-                  "material": "silentgear:meteorite_metal"
-                },
-                {
-                  "material": "silentgear:meteorite_metal"
-                }
+                { "material": "silentgear:meteorite_metal" },
+                { "material": "silentgear:meteorite_metal" }
               ]
             },
             "count": 1,
@@ -33,15 +23,269 @@ ServerEvents.recipes(event => {
           "item": {
             "components": {
               "silentgear:material_list": [
-                {
-                  "material": "silentgear:wood"
-                }
+                { "material": "silentgear:wood" }
               ]
             },
             "count": 1,
             "id": "silentgear:rod"
           },
           "part": "silentgear:rod"
+        }
+      ]
+    }
+  })
+
+  event.custom({
+    "type": "silentgear:conversion",
+    "category": "misc",
+    "ingredients": [{ "item": "roll_mod:meteorite_pickaxe" }],
+    "result": {
+      "item": "silentgear:pickaxe",
+      "parts": [
+        {
+          "item": {
+            "components": {
+              "silentgear:material_list": [
+                { "material": "silentgear:meteorite_metal" },
+                { "material": "silentgear:meteorite_metal" }
+              ]
+            },
+            "count": 1,
+            "id": "silentgear:pickaxe_head"
+          },
+          "part": "silentgear:pickaxe_head"
+        },
+        {
+          "item": {
+            "components": {
+              "silentgear:material_list": [
+                { "material": "silentgear:wood" }
+              ]
+            },
+            "count": 1,
+            "id": "silentgear:rod"
+          },
+          "part": "silentgear:rod"
+        }
+      ]
+    }
+  })
+
+  event.custom({
+    "type": "silentgear:conversion",
+    "category": "misc",
+    "ingredients": [{ "item": "roll_mod:meteorite_axe" }],
+    "result": {
+      "item": "silentgear:axe",
+      "parts": [
+        {
+          "item": {
+            "components": {
+              "silentgear:material_list": [
+                { "material": "silentgear:meteorite_metal" },
+                { "material": "silentgear:meteorite_metal" }
+              ]
+            },
+            "count": 1,
+            "id": "silentgear:axe_head"
+          },
+          "part": "silentgear:axe_head"
+        },
+        {
+          "item": {
+            "components": {
+              "silentgear:material_list": [
+                { "material": "silentgear:wood" }
+              ]
+            },
+            "count": 1,
+            "id": "silentgear:rod"
+          },
+          "part": "silentgear:rod"
+        }
+      ]
+    }
+  })
+
+  event.custom({
+    "type": "silentgear:conversion",
+    "category": "misc",
+    "ingredients": [{ "item": "roll_mod:meteorite_shovel" }],
+    "result": {
+      "item": "silentgear:shovel",
+      "parts": [
+        {
+          "item": {
+            "components": {
+              "silentgear:material_list": [
+                { "material": "silentgear:meteorite_metal" },
+                { "material": "silentgear:meteorite_metal" }
+              ]
+            },
+            "count": 1,
+            "id": "silentgear:shovel_head"
+          },
+          "part": "silentgear:shovel_head"
+        },
+        {
+          "item": {
+            "components": {
+              "silentgear:material_list": [
+                { "material": "silentgear:wood" }
+              ]
+            },
+            "count": 1,
+            "id": "silentgear:rod"
+          },
+          "part": "silentgear:rod"
+        }
+      ]
+    }
+  })
+
+  event.custom({
+    "type": "silentgear:conversion",
+    "category": "misc",
+    "ingredients": [{ "item": "roll_mod:meteorite_hoe" }],
+    "result": {
+      "item": "silentgear:hoe",
+      "parts": [
+        {
+          "item": {
+            "components": {
+              "silentgear:material_list": [
+                { "material": "silentgear:meteorite_metal" },
+                { "material": "silentgear:meteorite_metal" }
+              ]
+            },
+            "count": 1,
+            "id": "silentgear:hoe_head"
+          },
+          "part": "silentgear:hoe_head"
+        },
+        {
+          "item": {
+            "components": {
+              "silentgear:material_list": [
+                { "material": "silentgear:wood" }
+              ]
+            },
+            "count": 1,
+            "id": "silentgear:rod"
+          },
+          "part": "silentgear:rod"
+        }
+      ]
+    }
+  })
+
+  event.custom({
+    "type": "silentgear:conversion",
+    "category": "misc",
+    "ingredients": [{ "item": "roll_mod:meteorite_helmet" }],
+    "result": {
+      "item": "silentgear:helmet",
+      "parts": [
+        {
+          "item": {
+            "components": {
+              "silentgear:material_list": [
+                { "material": "silentgear:meteorite_metal" },
+                { "material": "silentgear:meteorite_metal" },
+                { "material": "silentgear:meteorite_metal" },
+                { "material": "silentgear:meteorite_metal" },
+                { "material": "silentgear:meteorite_metal" }
+              ]
+            },
+            "count": 1,
+            "id": "silentgear:helmet_plates"
+          },
+          "part": "silentgear:helmet_plates"
+        }
+      ]
+    }
+  })
+
+  event.custom({
+    "type": "silentgear:conversion",
+    "category": "misc",
+    "ingredients": [{ "item": "roll_mod:meteorite_chestplate" }],
+    "result": {
+      "item": "silentgear:chestplate",
+      "parts": [
+        {
+          "item": {
+            "components": {
+              "silentgear:material_list": [
+                { "material": "silentgear:meteorite_metal" },
+                { "material": "silentgear:meteorite_metal" },
+                { "material": "silentgear:meteorite_metal" },
+                { "material": "silentgear:meteorite_metal" },
+                { "material": "silentgear:meteorite_metal" },
+                { "material": "silentgear:meteorite_metal" },
+                { "material": "silentgear:meteorite_metal" },
+                { "material": "silentgear:meteorite_metal" }
+              ]
+            },
+            "count": 1,
+            "id": "silentgear:chestplate_plates"
+          },
+          "part": "silentgear:chestplate_plates"
+        }
+      ]
+    }
+  })
+
+  event.custom({
+    "type": "silentgear:conversion",
+    "category": "misc",
+    "ingredients": [{ "item": "roll_mod:meteorite_leggings" }],
+    "result": {
+      "item": "silentgear:leggings",
+      "parts": [
+        {
+          "item": {
+            "components": {
+              "silentgear:material_list": [
+                { "material": "silentgear:meteorite_metal" },
+                { "material": "silentgear:meteorite_metal" },
+                { "material": "silentgear:meteorite_metal" },
+                { "material": "silentgear:meteorite_metal" },
+                { "material": "silentgear:meteorite_metal" },
+                { "material": "silentgear:meteorite_metal" },
+                { "material": "silentgear:meteorite_metal" }
+              ]
+            },
+            "count": 1,
+            "id": "silentgear:legging_plates"
+          },
+          "part": "silentgear:legging_plates"
+        }
+      ]
+    }
+  })
+
+  event.custom({
+    "type": "silentgear:conversion",
+    "category": "misc",
+    "ingredients": [{ "item": "roll_mod:meteorite_boots" }],
+    "result": {
+      "item": "silentgear:boots",
+      "parts": [
+        {
+          "item": {
+            "components": {
+              "silentgear:material_list": [
+                { "material": "silentgear:meteorite_metal" },
+                { "material": "silentgear:meteorite_metal" },
+                { "material": "silentgear:meteorite_metal" },
+                { "material": "silentgear:meteorite_metal" }
+              ]
+            },
+            "count": 1,
+            "id": "silentgear:boot_plates"
+          },
+          "part": "silentgear:boot_plates"
         }
       ]
     }
