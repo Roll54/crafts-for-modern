383bc21916ca5c1b6aa7e0a6ac32939853a5bc3d
Roll54 <roll4248666@gmail.com>
2026-06-29 14:24:22 -0400

    fix hardcode

--- server_scripts/delete/ad_astra.js
+++ server_scripts/delete/ad_astra.js
@@ -1,66 +1,66 @@
 ServerEvents.recipes(event => {
-  event.remove({ output: "ad_astra:steel_engine" })
-  event.remove({ output: "ad_astra:oxygen_gear" })
-  event.remove({ output: "ad_astra:rocket_nose_cone" })
-  event.remove({ output: "ad_astra:ti_69" })
-  event.remove({ output: "ad_astra:etrionic_blast_furnace" })
-  event.remove({ output: "ad_astra:fuel_refinery" })
-  event.remove({ output: "ad_astra:oxygen_loader" })
-  event.remove({ output: "ad_astra:cable_duct" })
-  event.remove({ output: "ad_astra:fluid_pipe_duct" })
-  event.remove({ output: "ad_astra:oxygen_sensor" })
-  event.remove({ output: "ad_astra:calorite_engine" })
-  event.remove({ output: "ad_astra:calorite_ingot" })
-  event.remove({ output: "ad_astra:calorite_plate" })
-  event.remove({ output: "ad_astra:calorite_tank" })
-  event.remove({ output: "ad_astra:desh_cable" })
-  event.remove({ output: "ad_astra:desh_engine" })
-  event.remove({ output: "ad_astra:desh_ingot" })
-  event.remove({ output: "ad_astra:desh_plate" })
-  event.remove({ output: "ad_astra:desh_tank" })
-  event.remove({ output: "ad_astra:ostrum_plate" })
-  event.remove({ output: "ad_astra:ostrum_engine" })
-  event.remove({ output: "ad_astra:ostrum_ingot" })
-  event.remove({ output: "ad_astra:ostrum_tank" })
-  event.remove({ output: "ad_astra:radio" })
-  event.remove({ output: "ad_astra:jet_suit" })
-  event.remove({ output: "ad_astra:jet_suit_boots" })
-  event.remove({ output: "ad_astra:jet_suit_helmet" })
-  event.remove({ output: "ad_astra:jet_suit_pants" })
-  event.remove({ output: "ad_astra:netherite_space_boots" })
-  event.remove({ output: "ad_astra:netherite_space_helmet" })
-  event.remove({ output: "ad_astra:netherite_space_pants" })
-  event.remove({ output: "ad_astra:netherite_space_suit" })
-  event.remove({ output: "ad_astra:space_boots" })
-  event.remove({ output: "ad_astra:space_helmet" })
-  event.remove({ output: "ad_astra:space_pants" })
-  event.remove({ output: "ad_astra:space_suit" })
-  event.remove({ output: "ad_astra:oxygen_sensor" })
-  event.remove({ output: "ad_astra:cryo_freezer" })
-  event.remove({ output: "ad_astra:energizer" })
-  event.remove({ output: "ad_astra:gravity_normalizer" })
-  event.remove({ output: "ad_astra:oxygen_distributor" })
-  event.remove({ output: "ad_astra:water_pump" })
-  event.remove({ output: "ad_astra:solar_panel" })
-  event.remove({ output: "ad_astra:oxygen_loader" })
-  event.remove({ output: "ad_astra:nasa_workbench" })
-  event.remove({ output: "ad_astra:launch_pad" })
-  event.remove({ output: "ad_astra:cable_duct" })
-  event.remove({ output: "ad_astra:fluid_pipe_duct" })
-  event.remove({ output: "ad_astra:steel_cable" })
-  event.remove({ output: "ad_astra:desh_cable" })
-  event.remove({ output: "ad_astra:coal_generator" })
-  event.remove({ output: "ad_astra:compressor" })
-  event.remove({ output: "ad_astra:zip_gun" })
-  event.remove({ output: "ad_astra:gas_tank" })
-  event.remove({ output: "ad_astra:large_gas_tank" })
-  event.remove({ output: "ad_astra:tier_1_rover" })
-  event.remove({ output: "ad_astra:fan" })
-  event.remove({ output: "ad_astra:engine_frame" })
-  event.remove({ output: "ad_astra:wheel" })
-  event.remove({ output: "ad_astra:photovoltaic_etrium_cell" })
-  event.remove({ output: "ad_astra:etrionic_capacitor" })
-  event.remove({ output: "ad_astra:steel_block" })
+  
+const removedRecipes = [
+  "ad_astra:steel_engine",
+  "ad_astra:oxygen_gear",
+  "ad_astra:rocket_nose_cone",
+  "ad_astra:ti_69",
+  "ad_astra:etrionic_blast_furnace",
+  "ad_astra:fuel_refinery",
+  "ad_astra:oxygen_loader",
+  "ad_astra:cable_duct",
+  "ad_astra:fluid_pipe_duct",
+  "ad_astra:oxygen_sensor",
+  "ad_astra:calorite_engine",
+  "ad_astra:calorite_ingot",
+  "ad_astra:calorite_plate",
+  "ad_astra:calorite_tank",
+  "ad_astra:desh_cable",
+  "ad_astra:desh_engine",
+  "ad_astra:desh_ingot",
+  "ad_astra:desh_plate",
+  "ad_astra:desh_tank",
+  "ad_astra:ostrum_plate",
+  "ad_astra:ostrum_engine",
+  "ad_astra:ostrum_ingot",
+  "ad_astra:ostrum_tank",
+  "ad_astra:radio",
+  "ad_astra:jet_suit",
+  "ad_astra:jet_suit_boots",
+  "ad_astra:jet_suit_helmet",
+  "ad_astra:jet_suit_pants",
+  "ad_astra:netherite_space_boots",
+  "ad_astra:netherite_space_helmet",
+  "ad_astra:netherite_space_pants",
+  "ad_astra:netherite_space_suit",
+  "ad_astra:space_boots",
+  "ad_astra:space_helmet",
+  "ad_astra:space_pants",
+  "ad_astra:space_suit",
+  "ad_astra:cryo_freezer",
+  "ad_astra:energizer",
+  "ad_astra:gravity_normalizer",
+  "ad_astra:oxygen_distributor",
+  "ad_astra:water_pump",
+  "ad_astra:solar_panel",
+  "ad_astra:nasa_workbench",
+  "ad_astra:launch_pad",
+  "ad_astra:steel_cable",
+  "ad_astra:coal_generator",
+  "ad_astra:compressor",
+  "ad_astra:zip_gun",
+  "ad_astra:gas_tank",
+  "ad_astra:large_gas_tank",
+  "ad_astra:tier_1_rover",
+  "ad_astra:fan",
+  "ad_astra:engine_frame",
+  "ad_astra:wheel",
+  "ad_astra:photovoltaic_etrium_cell",
+  "ad_astra:etrionic_capacitor",
+  "ad_astra:steel_block"
+];
+
+removedRecipes.forEach(id => event.remove({ output: id }));
 
   event.remove({ id: "ad_astra:nasa_workbench/tier_1_rocket_from_nasa_workbench" })
   event.remove({ id: "ad_astra:nasa_workbench/tier_2_rocket_from_nasa_workbench" })
