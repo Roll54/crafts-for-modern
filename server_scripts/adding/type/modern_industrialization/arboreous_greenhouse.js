b55f776b50f3fc12eb1760f9f95e86aad26f64bb
LorpOOO <150320219+LorpOOO@users.noreply.github.com>
2026-06-30 07:39:12 +0300

    Update arboreous_greenhouse.js

--- server_scripts/adding/type/modern_industrialization/arboreous_greenhouse.js
+++ server_scripts/adding/type/modern_industrialization/arboreous_greenhouse.js
@@ -125,6 +125,186 @@ ServerEvents.recipes(event => {
         item_inputs: [
             {
                 amount: 1,
+                item: "minecraft:apple",
+                probability: 0.0
+            }
+        ],
+        item_outputs: [
+            {
+                amount: 8,
+                item: "minecraft:apple"
+            },
+            {
+                amount: 16,
+                item: "minecraft:oak_log"
+            },
+            {
+                amount: 32,
+                item: "fruitsdelight:apple_leaves"
+            },
+            {
+                amount: 1,
+                item: "fruitsdelight:apple_sapling",
+                probability: 0.5
+            }
+        ],
+        process_conditions: [
+            {
+                type: "yet_another_industrialization:arboreous_greenhouse_tier",
+                model: "fruitsdelight:apple_tree",
+                tier_id: "yet_another_industrialization:greenhouse_tier_1"
+            }
+        ]
+    });
+
+    event.custom({
+        type: "yet_another_industrialization:arboreous_greenhouse",
+        duration: 1200,
+        eu: 15,
+        fluid_inputs: [
+            {
+                amount: 250,
+                fluid: "extended_industrialization:distilled_water"
+            }
+        ],
+        item_inputs: [
+            {
+                amount: 1,
+                item: "minecraft:apple",
+                probability: 0.0
+            }
+        ],
+        item_outputs: [
+            {
+                amount: 16,
+                item: "minecraft:apple"
+            },
+            {
+                amount: 32,
+                item: "minecraft:oak_log"
+            },
+            {
+                amount: 64,
+                item: "fruitsdelight:apple_leaves"
+            },
+            {
+                amount: 1,
+                item: "fruitsdelight:apple_sapling",
+                probability: 1.0
+            }
+        ],
+        process_conditions: [
+            {
+                type: "yet_another_industrialization:arboreous_greenhouse_tier",
+                model: "fruitsdelight:apple_tree",
+                tier_id: "yet_another_industrialization:greenhouse_tier_2"
+            }
+        ]
+    });
+
+    event.custom({
+        type: "yet_another_industrialization:arboreous_greenhouse",
+        duration: 1200,
+        eu: 15,
+        fluid_inputs: [
+            {
+                amount: 1000,
+                fluid: "minecraft:water"
+            }
+        ],
+        item_inputs: [
+            {
+                amount: 1,
+                item: "fruitsdelight:lemon",
+                probability: 0.0
+            }
+        ],
+        item_outputs: [
+            {
+                amount: 6,
+                item: "fruitsdelight:lemon"
+            },
+            {
+                amount: 4,
+                item: "minecraft:oak_log"
+            },
+            {
+                amount: 8,
+                item: "fruitsdelight:lemon_tree"
+            },
+            {
+                amount: 1,
+                item: "fruitsdelight:lemon_seeds",
+                probability: 0.5
+            }
+        ],
+        process_conditions: [
+            {
+                type: "yet_another_industrialization:arboreous_greenhouse_tier",
+                model: "fruitsdelight:lemon_tree",
+                tier_id: "yet_another_industrialization:greenhouse_tier_1"
+            }
+        ]
+    });
+
+    event.custom({
+        type: "yet_another_industrialization:arboreous_greenhouse",
+        duration: 1200,
+        eu: 15,
+        fluid_inputs: [
+            {
+                amount: 250,
+                fluid: "extended_industrialization:distilled_water"
+            }
+        ],
+        item_inputs: [
+            {
+                amount: 1,
+                item: "fruitsdelight:lemon",
+                probability: 0.0
+            }
+        ],
+        item_outputs: [
+            {
+                amount: 12,
+                item: "fruitsdelight:lemon"
+            },
+            {
+                amount: 8,
+                item: "minecraft:oak_log"
+            },
+            {
+                amount: 16,
+                item: "fruitsdelight:lemon_tree"
+            },
+            {
+                amount: 1,
+                item: "fruitsdelight:lemon_seeds",
+                probability: 1.0
+            }
+        ],
+        process_conditions: [
+            {
+                type: "yet_another_industrialization:arboreous_greenhouse_tier",
+                model: "fruitsdelight:lemon_tree",
+                tier_id: "yet_another_industrialization:greenhouse_tier_2"
+            }
+        ]
+    });
+
+    event.custom({
+        type: "yet_another_industrialization:arboreous_greenhouse",
+        duration: 1200,
+        eu: 15,
+        fluid_inputs: [
+            {
+                amount: 1000,
+                fluid: "minecraft:water"
+            }
+        ],
+        item_inputs: [
+            {
+                amount: 1,
                 item: "fruitsdelight:blueberry",
                 probability: 0.0
             }
