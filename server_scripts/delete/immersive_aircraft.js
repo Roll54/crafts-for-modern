bea1f0f9d001deec2eea1b6943199bd4b2cd2e37
LorpOOO <150320219+LorpOOO@users.noreply.github.com>
2026-07-01 15:46:19 +0300

    Update immersive_aircraft.js

--- server_scripts/delete/immersive_aircraft.js
+++ server_scripts/delete/immersive_aircraft.js
@@ -1,16 +1,20 @@
 ServerEvents.recipes(event => {
+    const itemsToRemove = [
+        "immersive_aircraft:bomb_bay",
+        "immersive_aircraft:heavy_crossbow",
+        "immersive_aircraft:rotary_cannon",
+        "immersive_aircraft:airship",
+        "immersive_aircraft:cargo_airship",
+        "immersive_aircraft:warship",
+        "immersive_aircraft:gyrodyne",
+        "immersive_aircraft:biplane",
+        "immersive_aircraft:quadrocopter",
+        "immersive_aircraft:bamboo_hopper",
+        "immersive_aircraft:enhanced_propeller",
+        "immersive_aircraft:propeller"
+    ];
 
-event.remove({ output: "immersive_aircraft:bomb_bay" })
-event.remove({ output: "immersive_aircraft:heavy_crossbow" })
-event.remove({ output: "immersive_aircraft:rotary_cannon" })
-event.remove({ output: "immersive_aircraft:airship" })
-event.remove({ output: "immersive_aircraft:cargo_airship" })
-event.remove({ output: "immersive_aircraft:warship" })
-event.remove({ output: "immersive_aircraft:gyrodyne" })
-event.remove({ output: "immersive_aircraft:biplane" })
-event.remove({ output: "immersive_aircraft:quadrocopter" })
-event.remove({ output: "immersive_aircraft:bamboo_hopper" })
-event.remove({ output: "immersive_aircraft:enhanced_propeller" })
-event.remove({ output: "immersive_aircraft:propeller" })
-
-})
+    itemsToRemove.forEach(item => {
+        event.remove({ output: item });
+    });
+});
