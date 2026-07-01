9f4a4485c23082960158479baea4ddb38e96c84c
LorpOOO <150320219+LorpOOO@users.noreply.github.com>
2026-07-01 15:39:48 +0300

    Update general_remove_vanilla.js

--- server_scripts/delete/general_remove_vanilla.js
+++ server_scripts/delete/general_remove_vanilla.js
@@ -1,14 +1,15 @@
 ServerEvents.recipes(event => { 
 //REMOVE FOR SMALL MODS LIKE 2-3 ITEMS or vanilla
-  event.remove({ output: "minecraft:netherite_scrap" })
-  event.remove({ output: "minecraft:ender_eye" })
-  event.remove({ output: "blahaj:blue_shark" })
-  event.remove({ output: "tide:fishing_line" })
-  event.remove({ output: "silentgear:prospector_hammer_blueprint" })
-  event.remove({ output: "silentgear:prospector_hammer_template" })
-  event.remove({ output: "silentgear:advanced_upgrade_base" })
-  event.remove({ output: "ends_delight:end_stove" })
-  
-  event.remove({ mod: 'buildinggadgets2' })
-  event.remove({ mod: 'scannable_unofficial' })
+    event.remove({ output: "minecraft:netherite_scrap" })
+    event.remove({ output: "minecraft:ender_eye" })
+    event.remove({ output: "blahaj:blue_shark" })
+    event.remove({ output: "tide:fishing_line" })
+    event.remove({ output: "silentgear:prospector_hammer_blueprint" })
+    event.remove({ output: "silentgear:prospector_hammer_template" })
+    event.remove({ output: "silentgear:advanced_upgrade_base" })
+    event.remove({ output: "ends_delight:end_stove" })
+    
+    event.remove({ mod: 'buildinggadgets2' })
+    event.remove({ mod: 'scannable_unofficial' })
+    
 })
