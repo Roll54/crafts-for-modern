87eb75492cda85053c3a6696bc9ecede104a5b44
LorpOOO <150320219+LorpOOO@users.noreply.github.com>
2026-07-01 18:19:37 +0300

    Revert "Update compressor.js"
    This reverts commit e2466c213a6b978c8b92b4e7852cd14f732d4393.

--- server_scripts/adding/type/modern_industrialization/compressor.js
+++ server_scripts/adding/type/modern_industrialization/compressor.js
@@ -1,21 +1,21 @@
 ServerEvents.recipes(event => {
     event.recipes.modern_industrialization.compressor(2, 200)
         .itemIn("1x roll_mod:rubber_ingot")
-        .itemOut("2x modern_industrialization:rubber_sheet")
+        .itemOut("2x #c:plates/rubber")
 
     event.recipes.modern_industrialization.compressor(2, 800)
         .itemIn("5x #c:dusts/diamond")
-        .itemOut("2x modern_industrialization:diamond_plate")
+        .itemOut("2x #c:plates/diamond")
 
     event.recipes.modern_industrialization.compressor(2, 400)
         .itemIn("1x #c:ingots/ostrum")
-        .itemOut("1x ad_astra:ostrum_plate")
+        .itemOut("1x #c:plates/ostrum")
 
     event.recipes.modern_industrialization.compressor(2, 400)
         .itemIn("1x #c:ingots/desh")
-        .itemOut("1x ad_astra:desh_plate")
+        .itemOut("1x #c:plates/desh")
 
     event.recipes.modern_industrialization.compressor(2, 400)
         .itemIn("1x #c:ingots/calorite")
-        .itemOut("1x ad_astra:calorite_plate")
+        .itemOut("1x #c:plates/calorite")
 })
