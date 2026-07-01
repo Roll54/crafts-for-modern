c76d73dce80781f2bbcb2a5033be8ea5aa5f03f2
LorpOOO <150320219+LorpOOO@users.noreply.github.com>
2026-07-01 18:21:00 +0300

    Revert "Update canning_machine.js"
    This reverts commit a0495d93042f45036abae8b1a8a58aaf0ebebe0d.

--- server_scripts/adding/type/modern_industrialization/canning_machine.js
+++ server_scripts/adding/type/modern_industrialization/canning_machine.js
@@ -1,6 +1,11 @@
 ServerEvents.recipes(event => {
-  event.recipes.extended_industrialization.canning_machine(2, 100)
-  .fluidIn("1000x modern_industrialization:sulfuric_acid")
-  .itemIn("1x roll_mod:empty_primitive_battery")
-  .itemOut("1x roll_mod:primitive_battery")
+event.recipes.extended_industrialization.canning_machine(4, 200)
+.fluidIn("1000x modern_industrialization:sulfuric_acid")
+.itemIn("1x roll_mod:empty_primitive_battery")
+.itemOut("1x roll_mod:primitive_battery")
+
+//.fluidIn("20000x modern_industrialization:rocket_propellant")
+//.itemIn("1x roll_mod:fuel_canister")
+//.itemOut("1x roll_mod:rocket_fuel_canister")
+
 })
