e18245cf7b664314023fbc58fc40017b985b58a0
LorpOOO <150320219+LorpOOO@users.noreply.github.com>
2026-07-01 15:38:49 +0300

    Update general_remove_vanilla.js

--- server_scripts/delete/general_remove_vanilla.js
+++ server_scripts/delete/general_remove_vanilla.js
@@ -1,28 +1,14 @@
-ServerEvents.recipes(event => {
-
-
-  //REMOVE FOR SMALL MODS LIKE 2-3 ITEMS
-// or vanilla
-event.remove({ output: "minecraft:netherite_scrap" })
-event.remove({ output: "minecraft:ender_eye" })
-event.remove({ output: "travelanchors:travel_staff" })
-event.remove({ output: "travelanchors:travel_anchor" })
-event.remove({ output: "toms_storage:storage_terminal" })
-event.remove({ output: "toms_storage:crafting_terminal" })
-event.remove({ output: "toms_storage:trim" })
-event.remove({ output: "toms_storage:inventory_cable" })
-event.remove({ output: "toms_storage:inventory_interface" })
-event.remove({ output: "toms_storage:inventory_cable_connector" })
-event.remove({ output: "toms_storage:inventory_proxy" })
-event.remove({ output: "toms_storage:inventory_interface" })
-event.remove({ output: "toms_storage:inventory_connector" })
-event.remove({ output: "blahaj:blue_shark" })
-event.remove({ output: "tide:fishing_line" })
-event.remove({ output: "silentgear:prospector_hammer_blueprint" })
-event.remove({ output: "silentgear:prospector_hammer_template" })
-event.remove({ output: "silentgear:advanced_upgrade_base" })
-event.remove({ output: "ends_delight:end_stove" })
-
-event.remove({ mod: 'buildinggadgets2' })
-event.remove({ mod: 'scannable_unofficial' })
+ServerEvents.recipes(event => { 
+//REMOVE FOR SMALL MODS LIKE 2-3 ITEMS or vanilla
+  event.remove({ output: "minecraft:netherite_scrap" })
+  event.remove({ output: "minecraft:ender_eye" })
+  event.remove({ output: "blahaj:blue_shark" })
+  event.remove({ output: "tide:fishing_line" })
+  event.remove({ output: "silentgear:prospector_hammer_blueprint" })
+  event.remove({ output: "silentgear:prospector_hammer_template" })
+  event.remove({ output: "silentgear:advanced_upgrade_base" })
+  event.remove({ output: "ends_delight:end_stove" })
+  
+  event.remove({ mod: 'buildinggadgets2' })
+  event.remove({ mod: 'scannable_unofficial' })
 })
