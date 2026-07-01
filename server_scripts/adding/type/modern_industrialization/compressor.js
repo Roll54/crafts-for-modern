c23a710fcb8338f73c9693ba8a3bd9ee54364f0f
LorpOOO <150320219+LorpOOO@users.noreply.github.com>
2026-07-01 16:33:38 +0300

    Update compressor.js

--- server_scripts/adding/type/modern_industrialization/compressor.js
+++ server_scripts/adding/type/modern_industrialization/compressor.js
@@ -1,28 +1,21 @@
 ServerEvents.recipes(event => {
-  
-  event.recipes.modern_industrialization.compressor(2, 200)
-  .itemIn("1x roll_mod:rubber_ingot")
-  .itemOut("2x modern_industrialization:rubber_sheet")
+    event.recipes.modern_industrialization.compressor(2, 200)
+        .itemIn("1x roll_mod:rubber_ingot")
+        .itemOut("2x #c:plates/rubber")
 
-  event.recipes.modern_industrialization.compressor(2, 800)
-  .itemIn("5x modern_industrialization:diamond_dust")
-  .itemOut("2x modern_industrialization:diamond_plate")
+    event.recipes.modern_industrialization.compressor(2, 800)
+        .itemIn("5x #c:dusts/diamond")
+        .itemOut("2x #c:plates/diamond")
 
-  event.recipes.modern_industrialization.compressor(2, 400)
-    .itemIn("ad_astra:ostrum_ingot")
-    .itemOut("ad_astra:ostrum_plate")
+    event.recipes.modern_industrialization.compressor(2, 400)
+        .itemIn("1x #c:ingots/ostrum")
+        .itemOut("1x #c:plates/ostrum")
 
-  event.recipes.modern_industrialization.compressor(2, 400)
-    .itemIn("ad_astra:desh_ingot")
-    .itemOut("ad_astra:desh_plate")
+    event.recipes.modern_industrialization.compressor(2, 400)
+        .itemIn("1x #c:ingots/desh")
+        .itemOut("1x #c:plates/desh")
 
-  event.recipes.modern_industrialization.compressor(2, 400)
-    .itemIn("ad_astra:calorite_ingot")
-    .itemOut("ad_astra:calorite_plate")
-
-  /* Tech Reborn
-  event.recipes.modern_industrialization.compressor(32, 800)
-  .itemIn("8x kubejs:energium_dust")
-  .itemOut("1x kubejs:energium_tiny_crystal")
-  */
+    event.recipes.modern_industrialization.compressor(2, 400)
+        .itemIn("1x #c:ingots/calorite")
+        .itemOut("1x #c:plates/calorite")
 })
