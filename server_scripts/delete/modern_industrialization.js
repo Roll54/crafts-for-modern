dd7c9bdbdf681d1b17c9e96f7d8ccd947a7658e3
LorpOOO <150320219+LorpOOO@users.noreply.github.com>
2026-07-02 19:51:02 +0300

    Update modern_industrialization.js

--- server_scripts/delete/modern_industrialization.js
+++ server_scripts/delete/modern_industrialization.js
@@ -26,7 +26,8 @@ ServerEvents.recipes(event => {
         "modern_industrialization:stainless_steel_hot_ingot",
         "modern_industrialization:tungsten_cable",
         "modern_industrialization:electric_quarry",
-        "modern_industrialization:steam_quarry"
+        "modern_industrialization:steam_quarry",
+        "modern_industrialization:random_access_memory"
     ];
 
     const modernRecipes = [
