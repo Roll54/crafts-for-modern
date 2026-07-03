81ba813786369d230f75e27003005a9e1a4415fc
Roll54 <roll4248666@gmail.com>
2026-07-02 20:21:31 -0400

    vibed

--- server_scripts/adding/type/modern_industrialization/ore_washer.js
+++ server_scripts/adding/type/modern_industrialization/ore_washer.js
@@ -44,21 +44,29 @@ event.recipes.modern_industrialization.lv_ore_washer(32, 20)
 .fluidIn("modern_industrialization:mercury", 500)//todo ЛОРП НАДА ПАМІНЯТЬ ЦИФРИ МІСЦЯМИ БО ЦЕ ЛЕГАСІ КОД!
 .itemOut("1x roll_mod:purified_quartz_ore");
 
-function plantToOre (crushedOre, plantItem, numberOfPlants, outputOre, outputOreNumber, optionalOutput = null, optionalOutputNumber = 1 ){
+function plantToOre(
+  crushedOre,
+  plantItem,
+  numberOfPlants,
+  outputOre,
+  outputOreNumber,
+  optionalOutput = null,
+  optionalOutputNumber = 1
+) {
+  let recipe = event.recipes.modern_industrialization.lv_ore_washer(32, 60)
+    .itemIn(`1x ${crushedOre}`)
+    .itemIn(`${numberOfPlants}x ${plantItem}`)
+    .fluidIn(`1500x modern_industrialization:sodium_persulfate`)
+    .itemOut(`${outputOreNumber}x ${outputOre}`)
 
-    let recipe = event.recipes.modern_industrialization.lv_ore_washer(32, 60)
+  if (optionalOutput !== null) {
+    recipe.itemOut(`${optionalOutputNumber}x ${optionalOutput}`)
+  }
 
-    .itemIn('1x ${crushedOre}')
-    .itemIn('${numberOfPlants}x ${plantItem}')
-    .fluidIn('1500x modern_industrialization:sodium_persulfate')
-    .itemOut('${outputOreNumber}x ${outputOre}') 
-
-    if (optionalOutput !== null){
-
-    recipe.itemOut('${optionalOutputNumber}x ${optionalOutput}') 
-    
-    }
+  return recipe
 }
+
+
 
 plantToOre("roll_mod:crushed_nickel_ore", "roll_mod:nikelia_flowers", 20, "roll_mod:purified_nickel_ore", 4, "modern_industrialization:platinum_dust", 1)
 
