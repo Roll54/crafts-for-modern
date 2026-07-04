fd33608b24a1aee2ba819f42b29a2092d09f7441
LorpOOO <150320219+LorpOOO@users.noreply.github.com>
2026-07-04 10:17:16 +0300

    Update without_theme.js

--- server_scripts/adding/type/tags/without_theme.js
+++ server_scripts/adding/type/tags/without_theme.js
@@ -5,45 +5,47 @@ ServerEvents.tags('item', event => {
         "modern_industrialization:redstone_battery",
     ]);
 
-    // Додаємо до існуючого тегу ad_astra:freeze_resistant_armor
-    event.add('ad_astra:freeze_resistant_armor', [
-        "extended_industrialization:nano_quantum_leggings",
-        "extended_industrialization:nano_quantum_chestplate",
-        "extended_industrialization:nano_quantum_boots",
-        "extended_industrialization:nano_quantum_helmet",
-        "ad_astra:netherite_space_helmet",
-        "ad_astra:netherite_space_suit",
-        "ad_astra:netherite_space_pants",
-        "ad_astra:netherite_space_boots",
-        "ad_astra:jet_suit_helmet",
-        "ad_astra:jet_suit",
-        "ad_astra:jet_suit_pants",
-        "ad_astra:jet_suit_boots",
-        "advanced_ae:quantum_helmet",
-        "advanced_ae:quantum_chestplate",
-        "advanced_ae:quantum_leggings",
-        "advanced_ae:quantum_boots",
-    ]);
+    event.modify('ad_astra:freeze_resistant_armor', tag => {
+        tag.add([
+            "extended_industrialization:nano_quantum_leggings",
+            "extended_industrialization:nano_quantum_chestplate",
+            "extended_industrialization:nano_quantum_boots",
+            "extended_industrialization:nano_quantum_helmet",
+            "ad_astra:netherite_space_helmet",
+            "ad_astra:netherite_space_suit",
+            "ad_astra:netherite_space_pants",
+            "ad_astra:netherite_space_boots",
+            "ad_astra:jet_suit_helmet",
+            "ad_astra:jet_suit",
+            "ad_astra:jet_suit_pants",
+            "ad_astra:jet_suit_boots",
+            "advanced_ae:quantum_helmet",
+            "advanced_ae:quantum_chestplate",
+            "advanced_ae:quantum_leggings",
+            "advanced_ae:quantum_boots",
+        ]);
+    });
 
-    // Додаємо до існуючого тегу ad_astra:space_suit_items
-    event.add('ad_astra:space_suit_items', [
-        "extended_industrialization:nano_quantum_leggings",
-        "extended_industrialization:nano_quantum_chestplate",
-        "extended_industrialization:nano_quantum_boots",
-        "extended_industrialization:nano_quantum_helmet",
-        "ad_astra:netherite_space_helmet",
-        "ad_astra:netherite_space_suit",
-        "ad_astra:netherite_space_pants",
-        "ad_astra:netherite_space_boots",
-        "ad_astra:jet_suit_helmet",
-        "ad_astra:jet_suit",
-        "ad_astra:jet_suit_pants",
-        "ad_astra:jet_suit_boots",
-        "advanced_ae:quantum_helmet",
-        "advanced_ae:quantum_chestplate",
-        "advanced_ae:quantum_leggings",
-        "advanced_ae:quantum_boots",
-    ]);
+    event.modify('ad_astra:space_suit_items', tag => {
+        tag.add([
+            "extended_industrialization:nano_quantum_leggings",
+            "extended_industrialization:nano_quantum_chestplate",
+            "extended_industrialization:nano_quantum_boots",
+            "extended_industrialization:nano_quantum_helmet",
+            "ad_astra:netherite_space_helmet",
+            "ad_astra:netherite_space_suit",
+            "ad_astra:netherite_space_pants",
+            "ad_astra:netherite_space_boots",
+            "ad_astra:jet_suit_helmet",
+            "ad_astra:jet_suit",
+            "ad_astra:jet_suit_pants",
+            "ad_astra:jet_suit_boots",
+            "advanced_ae:quantum_helmet",
+            "advanced_ae:quantum_chestplate",
+            "advanced_ae:quantum_leggings",
+            "advanced_ae:quantum_boots",
+        ]);
+    });
 
     event.add('roll_mod:good_for_crystal_chip', [
         "roll_mod:raw_emerald",
