ef270ef2e73224a0c6b80291105454923983443b
LorpOOO <150320219+LorpOOO@users.noreply.github.com>
2026-07-04 10:03:51 +0300

    Update without_theme.js

--- server_scripts/adding/type/tags/without_theme.js
+++ server_scripts/adding/type/tags/without_theme.js
@@ -6,6 +6,48 @@ event.add('roll_mod:battaries_tier_one', [
     
 ]);
 
+event.add('ad_astra:freeze_resistant_armor', [
+    "extended_industrialization:nano_quantum_leggings",
+    "extended_industrialization:nano_quantum_chestplate",
+    "extended_industrialization:nano_quantum_boots",
+    "extended_industrialization:nano_quantum_helmet"
+    "extended_industrialization:nano_quantum_helmet",
+    "ad_astra:netherite_space_helmet",
+    "ad_astra:netherite_space_suit",
+    "ad_astra:netherite_space_pants",
+    "ad_astra:netherite_space_boots",
+    "ad_astra:jet_suit_helmet",
+    "ad_astra:jet_suit",
+    "ad_astra:jet_suit_pants",
+    "ad_astra:jet_suit_boots",
+    "advanced_ae:quantum_helmet",
+    "advanced_ae:quantum_chestplate",
+    "advanced_ae:quantum_leggings",
+    "advanced_ae:quantum_boots",
+    
+]);
+
+event.add('ad_astra:space_suit_items', [
+    "extended_industrialization:nano_quantum_leggings",
+    "extended_industrialization:nano_quantum_chestplate",
+    "extended_industrialization:nano_quantum_boots",
+    "extended_industrialization:nano_quantum_helmet"
+    "extended_industrialization:nano_quantum_helmet",
+    "ad_astra:netherite_space_helmet",
+    "ad_astra:netherite_space_suit",
+    "ad_astra:netherite_space_pants",
+    "ad_astra:netherite_space_boots",
+    "ad_astra:jet_suit_helmet",
+    "ad_astra:jet_suit",
+    "ad_astra:jet_suit_pants",
+    "ad_astra:jet_suit_boots",
+    "advanced_ae:quantum_helmet",
+    "advanced_ae:quantum_chestplate",
+    "advanced_ae:quantum_leggings",
+    "advanced_ae:quantum_boots",
+    
+]);
+
 event.add('roll_mod:good_for_crystal_chip', [
 "roll_mod:raw_emerald",
 "roll_mod:raw_olivine",
