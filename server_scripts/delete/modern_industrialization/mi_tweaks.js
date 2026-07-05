cff3b31f3c2de316f6fe01066ba6469513164e81
LorpOOO <150320219+LorpOOO@users.noreply.github.com>
2026-07-05 21:15:16 +0300

    Update mi_tweaks.js

--- server_scripts/delete/modern_industrialization/mi_tweaks.js
+++ server_scripts/delete/modern_industrialization/mi_tweaks.js
@@ -1,1 +1,9 @@
+ServerEvents.recipes(event => {
+    const addonItems = [
+        "mi_tweaks:flux_transformer",
+    ];
 
+    addonItems.forEach(item => {
+        event.remove({ output: item });
+    });
+});
