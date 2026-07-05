33a8077af3b7667a55a3cb68324625695c1eae5e
LorpOOO <150320219+LorpOOO@users.noreply.github.com>
2026-07-05 21:15:02 +0300

    Update extended_industrialization.js

--- server_scripts/delete/modern_industrialization/extended_industrialization.js
+++ server_scripts/delete/modern_industrialization/extended_industrialization.js
@@ -1,1 +1,19 @@
+ServerEvents.recipes(event => {
+    const addonItems = [
+        "extended_industrialization:electric_mining_drill",
+        "extended_industrialization:electric_chainsaw",
+        "extended_industrialization:nano_helmet",
+        "extended_industrialization:nano_chestplate",
+        "extended_industrialization:nano_leggings",
+        "extended_industrialization:nano_boots",
+        "extended_industrialization:nano_gravichestplate",
+        "extended_industrialization:nano_saber",
+        "extended_industrialization:lv_solar_panel",
+        "extended_industrialization:mv_solar_panel",
+        "extended_industrialization:hv_solar_panel",
+    ];
 
+    addonItems.forEach(item => {
+        event.remove({ output: item });
+    });
+});
