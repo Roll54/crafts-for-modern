a5fe636233eb6b0192dc1f8ef5c3ce6b10ac2753
LorpOOO <150320219+LorpOOO@users.noreply.github.com>
2026-07-05 21:14:45 +0300

    Update industrialization_overdrive.js

--- server_scripts/delete/modern_industrialization/industrialization_overdrive.js
+++ server_scripts/delete/modern_industrialization/industrialization_overdrive.js
@@ -1,1 +1,19 @@
+ServerEvents.recipes(event => {
+    const addonItems = [
+        "industrialization_overdrive:vajra",
+        "industrialization_overdrive:multi_processing_array",
+        "industrialization_overdrive:pyrolyse_oven",
+    ];
 
+    const addonRecipes = [
+        "industrialization_overdrive:pyrolyse_oven",
+    ];
+
+    addonItems.forEach(item => {
+        event.remove({ output: item });
+    });
+
+    addonRecipes.forEach(id => {
+        event.remove({ id: id });
+    });
+});
