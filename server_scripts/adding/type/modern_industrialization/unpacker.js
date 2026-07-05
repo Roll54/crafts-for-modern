1840890103b9222cbffb5f5d1e5a4523c3595ad2
LorpOOO <150320219+LorpOOO@users.noreply.github.com>
2026-07-05 13:40:10 +0300

    Update unpacker.js

--- server_scripts/adding/type/modern_industrialization/unpacker.js
+++ server_scripts/adding/type/modern_industrialization/unpacker.js
@@ -1,6 +1,17 @@
 ServerEvents.recipes(event => {
+  
   event.recipes.modern_industrialization.unpacker(2, 100)
-  .itemIn("1x extended_industrialization:nano_gravichestplate")
-  .itemOut("1x extended_industrialization:nano_chestplate")
-  .itemOut("1x roll_mod:multi_protecting_gravi_chestplate")
+    .itemIn("1x extended_industrialization:nano_gravichestplate")
+    .itemOut("1x extended_industrialization:nano_chestplate")
+    .itemOut("1x roll_mod:multi_protecting_gravi_chestplate")
+
+  event.recipes.modern_industrialization.packer(2, 100)
+    .itemIn("9x #c:dusts/sulfur")
+    .itemIn("1x modern_industrialization:packer_block_template", 0.0)
+    .itemOut("1x modern_industrialization:sulfur_block")
+  
+  event.recipes.modern_industrialization.packer(2, 100)
+    .itemIn("9x #c:tiny_dusts/sulfur")
+    .itemOut("1x roll_mod:sulfur_dust")
+  
 })
