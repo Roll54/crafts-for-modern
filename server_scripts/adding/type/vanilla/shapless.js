670146cb95d2ff3ee4673176fc877735e8f88604
LorpOOO <150320219+LorpOOO@users.noreply.github.com>
2026-07-05 13:31:53 +0300

    Update shapless.js

--- server_scripts/adding/type/vanilla/shapless.js
+++ server_scripts/adding/type/vanilla/shapless.js
@@ -1,6 +1,13 @@
 ServerEvents.recipes(event => {
 
 event.shapeless(
+    Item.of('modern_industrialization:sulfur_dust'),
+    [
+        'roll_mod:sulfur_dust'
+    ]
+)
+    
+event.shapeless(
     Item.of('modern_industrialization:beryllium_tiny_dust', 9),
     [
         'roll_mod:beryllium_dust'
