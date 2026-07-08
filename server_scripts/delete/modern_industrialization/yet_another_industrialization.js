e8cd0c0f9a4b20604e4002b920886e948caba881
LorpOOO <150320219+LorpOOO@users.noreply.github.com>
2026-07-08 23:01:30 +0300

    Update yet_another_industrialization.js

--- server_scripts/delete/modern_industrialization/yet_another_industrialization.js
+++ server_scripts/delete/modern_industrialization/yet_another_industrialization.js
@@ -1,1 +1,9 @@
-yet_another_industrialization:flight_pylon
+ServerEvents.recipes(event => {
+    const addonItems = [
+        "yet_another_industrialization:flight_pylon",
+    ];
+
+    addonItems.forEach(item => {
+        event.remove({ output: item });
+    });
+});
