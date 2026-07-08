0e481bae0aaf79dcff07366e319a14590e879f2d
LorpOOO <150320219+LorpOOO@users.noreply.github.com>
2026-07-08 21:04:39 +0300

    Update advanced_ae.js

--- server_scripts/delete/applied_energystics/advanced_ae.js
+++ server_scripts/delete/applied_energystics/advanced_ae.js
@@ -1,7 +1,43 @@
 ServerEvents.recipes(event => {
+    const advancedItems = [
+        "advanced_ae:quantum_helmet",
+        "advanced_ae:quantum_chestplate",
+        "advanced_ae:quantum_leggings",
+        "advanced_ae:quantum_boots",
+        "advanced_ae:quantum_upgrade_base",
+        "advanced_ae:swim_speed_card",
+        "advanced_ae:jump_height_card",
+        "advanced_ae:step_assist_card",
+        "advanced_ae:sprint_speed_card",
+        "advanced_ae:walk_speed_card",
+        "advanced_ae:evasion_card",
+        "advanced_ae:strength_card",
+        "advanced_ae:attack_speed_card",
+        "advanced_ae:luck_card",
+        "advanced_ae:reach_card",
+        "advanced_ae:night_vision_card",
+        "advanced_ae:lava_immunity_card",
+        "advanced_ae:flight_card",
+        "advanced_ae:water_breathing_card",
+        "advanced_ae:auto_feeding_card",
+        "advanced_ae:auto_stock_card",
+        "advanced_ae:magnet_card",
+        "advanced_ae:recharging_card",
+        "advanced_ae:flight_drift_card",
+        "advanced_ae:portable_workbench_card",
+        "advanced_ae:pick_craft_card",
+        "advanced_ae:regeneration_card",
+        "advanced_ae:hp_buffer_card",
+        "advanced_ae:quantum_upgrade_base",
+    ];
+  
+    advancedItems.forEach(item => {
+        event.remove({ output: item });
+    });
 
-event.remove({ type: "advanced_ae:reaction" })
-//just AE2 
-//TODO MAKE ANOTHER FILE
-  event.remove({output: 'ae2:spatial_anchor'})
-})
+    rawQuantumItems.forEach(item => {
+        event.remove({ output: item });
+    });
+
+    event.remove({ type: "advanced_ae:reaction" });
+});
