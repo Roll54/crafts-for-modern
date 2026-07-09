782f005331006e7850fc6cd15bfeb95f245c43e4
LorpOOO <150320219+LorpOOO@users.noreply.github.com>
2026-07-09 09:49:36 +0300

    Update vacuum_freezer.js

--- server_scripts/adding/type/modern_industrialization/vacuum_freezer.js
+++ server_scripts/adding/type/modern_industrialization/vacuum_freezer.js
@@ -1,16 +1,16 @@
 ServerEvents.recipes(event => {
 
   event.recipes.modern_industrialization.vacuum_freezer(8, 100)
-  .fluidIn("modern_industrialization:polytetrafluoroethylene", 100)
-  .itemOut("1x modern_industrialization:polytetrafluoroethylene_ingot")
+    .fluidIn("modern_industrialization:polytetrafluoroethylene", 100)
+    .itemOut("1x modern_industrialization:polytetrafluoroethylene_ingot")
 
   event.recipes.modern_industrialization.vacuum_freezer(128, 200)
-  .biome("ad_astra:orbit")
-  .itemIn("1x yet_another_industrialization:tempproof_air_intake", 0.50)
-  .fluidOut"modern_industrialization:moon_air", 1000)
+    .biome("ad_astra:orbit")
+    .itemIn("1x yet_another_industrialization:tempproof_air_intake", 0.50)
+    .fluidOut("modern_industrialization:moon_air", 1000)
 
   event.recipes.modern_industrialization.vacuum_freezer(32, 1600)
-  .itemIn("1x roll_mod:superconducting_magnet_base")
-  .itemOut("1x roll_mod:superconducting_magnet")
+    .itemIn("1x roll_mod:superconducting_magnet_base")
+    .itemOut("1x roll_mod:superconducting_magnet")
     
 })
