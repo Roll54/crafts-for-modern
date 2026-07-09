33e9b323ae17360dd040726c6374d5eb2a02e854
LorpOOO <150320219+LorpOOO@users.noreply.github.com>
2026-07-09 08:49:13 +0300

    Update vacuum_freezer.js

--- server_scripts/adding/type/modern_industrialization/vacuum_freezer.js
+++ server_scripts/adding/type/modern_industrialization/vacuum_freezer.js
@@ -4,6 +4,10 @@ ServerEvents.recipes(event => {
   .fluidIn("modern_industrialization:polytetrafluoroethylene", 100)
   .itemOut("1x modern_industrialization:polytetrafluoroethylene_ingot")
 
+  event.recipes.modern_industrialization.vacuum_freezer(128, 200)
+  .itemOut("1x yet_another_industrialization:tempproof_air_intake", 0.50)
+  .fluidOut"modern_industrialization:moon_air", 1000)
+
   event.recipes.modern_industrialization.vacuum_freezer(32, 1600)
   .itemIn("1x roll_mod:superconducting_magnet_base")
   .itemOut("1x roll_mod:superconducting_magnet")
