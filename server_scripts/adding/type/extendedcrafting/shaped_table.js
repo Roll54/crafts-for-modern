86061d910724b81e8206415439085fabec75317d
LorpOOO <150320219+LorpOOO@users.noreply.github.com>
2026-07-09 08:25:47 +0300

    Update shaped_table.js

--- server_scripts/adding/type/extendedcrafting/shaped_table.js
+++ server_scripts/adding/type/extendedcrafting/shaped_table.js
@@ -23,9 +23,8 @@ ServerEvents.recipes(event => {
             "P": {
                 item: "modern_industrialization:quantum_circuit"
             },
-            "C": {
-                // Використовуємо Item.of() для коректного зчитування компонентів в KubeJS
-                item: Item.of('roll_mod:lapotron_battery_t3', '{ "modern_industrialization:energy": 500000000000 }').toJson()
+            "С": {
+                item: "roll_mod:lapotron_battery_t3"
             }
         },
         result: {
