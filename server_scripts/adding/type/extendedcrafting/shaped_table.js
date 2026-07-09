f36dedbb39659e619e1df18540be9656d7f33b83
LorpOOO <150320219+LorpOOO@users.noreply.github.com>
2026-07-09 08:11:26 +0300

    Update shaped_table.js

--- server_scripts/adding/type/extendedcrafting/shaped_table.js
+++ server_scripts/adding/type/extendedcrafting/shaped_table.js
@@ -1,1 +1,33 @@
+ServerEvents.recipes(event => {
 
+    event.custom({
+        type: 'extendedcrafting:shaped_table',
+        pattern: [
+            "Y  Y  Y  ",
+            " YYYYYYY ",
+            " YGYYYG  ",
+            " YYGCGYY ",
+            "YYY C  YY",
+            " YYGCGYY ",
+            " YGYYYG  ",
+            " YYYYYYY ",
+            "Y  Y  Y  "
+        ],
+        key: {
+            "Y": {
+                item: "c:gems/diamond"
+            },
+            "G": {
+                item: "c:gems/lapis"
+            },
+            "C": {
+                item: "roll_mod:lapotron_battery_t3[modern_industrialization:energy=500000000000L]"
+            }
+        },
+        result: {
+            id: "roll_mod:ultra_battery",
+            count: 1
+        }
+    })
+
+})
