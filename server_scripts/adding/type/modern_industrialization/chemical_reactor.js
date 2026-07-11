d21798e8b48c998869c2d39ff6150e2542535839
LorpOOO <150320219+LorpOOO@users.noreply.github.com>
2026-07-11 20:09:18 +0300

    Update chemical_reactor.js

--- server_scripts/adding/type/modern_industrialization/chemical_reactor.js
+++ server_scripts/adding/type/modern_industrialization/chemical_reactor.js
@@ -2,7 +2,7 @@ ServerEvents.recipes(event => {
 
 /*
 event.recipes.modern_industrialization.chemical_reactor(32, 200)
-.itemIn("1x roll_mod:unactivated_zinc_sulfide_dust")
+.itemIn("1x roll_mod:unactivated_zinc_sulfide_powder")
 .itemIn("1x #c:dusts/copper")
 .itemIn("1x #c:dusts/salf")
 .itemOut("1x roll_mod:luminescent_mixture")
