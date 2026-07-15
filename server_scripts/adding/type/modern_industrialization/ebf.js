836ac3ad3acdcdff3bbd9757040bed7f2a65bd35
LorpOOO <150320219+LorpOOO@users.noreply.github.com>
2026-07-15 18:57:55 +0300

    Update ebf.js

--- server_scripts/adding/type/modern_industrialization/ebf.js
+++ server_scripts/adding/type/modern_industrialization/ebf.js
@@ -1,7 +1,7 @@
 ServerEvents.recipes(event => {
 
   event.recipes.modern_industrialization.blast_furnace(64, 400)
-  .itemIn("1x roll_mod:synthetic_glowstone_dust")
+  .itemIn("1x roll_mod:zinc_sulfur_mixture")
   .itemOut("1x roll_mod:unactivated_zinc_sulfide_powder")
 
   event.recipes.modern_industrialization.blast_furnace(64, 400)
