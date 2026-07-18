2d6b48f2cb7774392f8bd9248d57cf68fa8c9963
LorpOOO <150320219+LorpOOO@users.noreply.github.com>
2026-07-18 20:45:27 +0300

    Update canning_machine.js

--- server_scripts/adding/type/modern_industrialization/canning_machine.js
+++ server_scripts/adding/type/modern_industrialization/canning_machine.js
@@ -1,6 +1,11 @@
 ServerEvents.recipes(event => {
-event.recipes.extended_industrialization.canning_machine(4, 200)
-.fluidIn("1000x modern_industrialization:sulfuric_acid")
-.itemIn("1x roll_mod:empty_primitive_battery")
-.itemOut("1x roll_mod:primitive_battery")
+  event.recipes.extended_industrialization.canning_machine(4, 200)
+  .fluidIn("1000x modern_industrialization:sulfuric_acid")
+  .itemIn("1x roll_mod:empty_primitive_battery")
+  .itemOut("1x roll_mod:primitive_battery")
+  
+  event.recipes.extended_industrialization.canning_machine(4, 200)
+  .fluidIn("200x sophisticatedcore:xp_still")
+  .itemIn("1x fruitsdelight:jam_bread")
+  .itemOut("1x roll_mod:xp_burger")
 })
