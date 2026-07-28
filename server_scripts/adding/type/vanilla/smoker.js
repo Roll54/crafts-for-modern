25f31b75592372b8b97ae04902c8db2aa12a3c06
LorpOOO <150320219+LorpOOO@users.noreply.github.com>
2026-07-28 19:10:35 +0300

    Update smoker.js

--- server_scripts/adding/type/vanilla/smoker.js
+++ server_scripts/adding/type/vanilla/smoker.js
@@ -4,4 +4,5 @@ ServerEvents.recipes(event => {
     event.smoking('createcybernetics:cooked_liver', 'createcybernetics:bodypart_liver').xp(0.35)
     event.smoking('createcybernetics:roasted_andouille', 'createcybernetics:andouille_sausage').xp(0.35)
     event.smoking('createcybernetics:bone_marrow', 'minecraft:bone').xp(0.35)
+    event.smoking('roll_mod:roasted_bell_pepper_rutile', 'roll_mod:rutile_bell_pepper').xp(0.35)
 })
