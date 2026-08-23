0957c515517f2c3405ddebe8508f3dd9dac4615d
LorpOOO <150320219+LorpOOO@users.noreply.github.com>
2026-08-23 23:16:21 +0300

    Update functionalstorage.js

--- server_scripts/delete/functionalstorage.js
+++ server_scripts/delete/functionalstorage.js
@@ -1,1 +1,18 @@
+ServerEvents.recipes(event => {
+    const removeItems = [
+        "functionalstorage_three_drawers:framed_fluid_3",
+        "functionalstorage:fluid_4",
+        "functionalstorage_three_drawers:fluid_3",
+        "functionalstorage:fluid_2",
+        "functionalstorage:fluid_1",
+        "functionalstorage:framed_fluid_4",
+        "functionalstorage:framed_fluid_2",
+        "functionalstorage:framed_fluid_1",
+        "functionalstorage:dripping_upgrade",
+        "functionalstorage:water_generator_upgrade"
+    ];
 
+    removeItems.forEach(item => {
+        event.remove({ output: item });
+    });
+});
