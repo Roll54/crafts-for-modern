2df994d0ef889e638332a7e9747bf99ddcd3b37d
LorpOOO <150320219+LorpOOO@users.noreply.github.com>
2026-09-02 20:27:36 +0300

    Update vanilla.js

--- server_scripts/changing/vanilla/vanilla.js
+++ server_scripts/changing/vanilla/vanilla.js
@@ -1,15 +1,63 @@
 ServerEvents.recipes(event => {
 
 event.replaceInput(
-  { input: 'minecraft:diamond_shovel' },
-  'minecraft:diamond_shovel',
-  '#minecraft:shovels'
-)
-
-event.replaceInput(
   { input: 'minecraft:redstone' },
   'minecraft:redstone',
   '#c:dusts/redstone'
+)
+
+event.replaceInput(
+  { output: 'minecraft:diamond_helmet' },
+  'minecraft:diamond',
+  'roll_mod:diamond_alloy_plate'
+)
+
+event.replaceInput(
+  { output: 'minecraft:diamond_chestplate' },
+  'minecraft:diamond',
+  'roll_mod:diamond_alloy_plate'
+)
+
+event.replaceInput(
+  { output: 'minecraft:diamond_leggings' },
+  'minecraft:diamond',
+  'roll_mod:diamond_alloy_plate'
+)
+
+event.replaceInput(
+  { output: 'minecraft:diamond_boots' },
+  'minecraft:diamond',
+  'roll_mod:diamond_alloy_plate'
+)
+
+event.replaceInput(
+  { output: 'minecraft:diamond_pickaxe' },
+  'minecraft:diamond',
+  'roll_mod:diamond_alloy_plate'
+)
+
+event.replaceInput(
+  { output: 'minecraft:diamond_axe' },
+  'minecraft:diamond',
+  'roll_mod:diamond_alloy_plate'
+)
+
+event.replaceInput(
+  { output: 'minecraft:diamond_shovel' },
+  'minecraft:diamond',
+  'roll_mod:diamond_alloy_plate'
+)
+
+event.replaceInput(
+  { output: 'minecraft:diamond_sword' },
+  'minecraft:diamond',
+  'roll_mod:diamond_alloy_plate'
+)
+
+event.replaceInput(
+  { output: 'minecraft:diamond_hoe' },
+  'minecraft:diamond',
+  'roll_mod:diamond_alloy_plate'
 )
 
 })
