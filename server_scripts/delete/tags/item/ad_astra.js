cab4e2cf7e66dfeadacddadf88d3f5540bd66c86
LorpOOO <150320219+LorpOOO@users.noreply.github.com>
2026-09-05 00:36:35 +0300

    Update ad_astra.js

--- server_scripts/delete/tags/item/ad_astra.js
+++ server_scripts/delete/tags/item/ad_astra.js
@@ -1,6 +1,9 @@
 ServerEvents.tags('item', event => {
   event.remove('c:plates/iron', 'ad_astra:iron_plate')
   event.remove('c:plates/steel', 'ad_astra:steel_plate')
+  
   event.remove('ad_astra:iron_plates', 'ad_astra:iron_plate')
   event.remove('ad_astra:steel_plates', 'ad_astra:steel_plate')
+
+  event.remove('c:ingots/stee', 'ad_astra:steel_ingot')
 })
