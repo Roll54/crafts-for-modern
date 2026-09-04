b48b3710b0733aacbacd6ae1b95b4b9a5b6b0a6d
LorpOOO <150320219+LorpOOO@users.noreply.github.com>
2026-09-05 00:19:32 +0300

    Update test.js

--- server_scripts/delete/tags/test.js
+++ server_scripts/delete/tags/test.js
@@ -3,6 +3,34 @@ ServerEvents.tags('block', event => {
   event.remove('minecraft:needs_stone_tool', 'modern_industrialization:deepslate_iridium_ore');
   event.remove('minecraft:mineable/pickaxe', 'modern_industrialization:deepslate_iridium_ore');
 
+  event.remove('c:ingots/lead', 'railcraft:lead_ingot')
+  event.remove('c:ingots/nickel', 'railcraft:nickel_ingot')
+  event.remove('c:ingots/silver', 'railcraft:silver_ingot')
+  event.remove('c:ingots/tin', 'railcraft:tin_ingot')
+  event.remove('c:ingots/zinc', 'railcraft:zinc_ingot')
+  event.remove('c:ingots/steel', 'railcraft:steel_ingot')
+  event.remove('c:ingots/brass', 'railcraft:brass_ingot')
+  event.remove('c:ingots/bronze', 'railcraft:bronze_ingot')
+  event.remove('c:ingots/invar', 'railcraft:invar_ingot')
+  
+  event.remove('c:nuggets/invar', 'railcraft:invar_nugget')
+  event.remove('c:nuggets/bronze', 'railcraft:bronze_nugget')
+  event.remove('c:nuggets/brass', 'railcraft:brass_nugget')
+  event.remove('c:nuggets/steel', 'railcraft:steel_nugget')
+  event.remove('c:nuggets/zinc', 'railcraft:zinc_nugget')
+  event.remove('c:nuggets/tin', 'railcraft:tin_nugget')
+  event.remove('c:nuggets/silver', 'railcraft:silver_nugget')
+  event.remove('c:nuggets/nickel', 'railcraft:nickel_nugget')
+  event.remove('c:nuggets/lead', 'railcraft:lead_nugget')
+  
+  event.remove('c:dusts/saltpeter', 'railcraft:saltpeter_dust')
+  event.remove('c:dusts/coal', 'railcraft:coal_dust')
+  event.remove('c:dusts/charcoal', 'railcraft:charcoal_dust')
+  event.remove('c:dusts/slag', 'railcraft:slag')
+  event.remove('c:dusts/ender', 'railcraft:ender_dust')
+  event.remove('c:dusts/sulfur', 'railcraft:sulfur_dust')
+  event.remove('c:dusts/obsidian', 'railcraft:obsidian_dust')
+
   event.remove('c:gears/brass', 'railcraft:brass_gear')
   event.remove('c:gears/bronze', 'railcraft:bronze_gear')
   event.remove('c:gears/invar', 'railcraft:invar_gear')
