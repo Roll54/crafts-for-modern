f00a1fe0c07b08700280fc76dc760e7b87f6e312
LorpOOO <150320219+LorpOOO@users.noreply.github.com>
2026-09-05 09:49:57 +0300

    Update ad_astra.js

--- server_scripts/delete/tags/item/ad_astra.js
+++ server_scripts/delete/tags/item/ad_astra.js
@@ -10,4 +10,11 @@ ServerEvents.tags('item', event => {
 
   event.remove('c:ingots/nuggets', 'ad_astra:steel_nugget')
   event.remove('ad_astra:steel_nuggets', 'ad_astra:steel_nugget')
+
+  event.remove('c:rods/steel', 'ad_astra:steel_rod')
+  event.remove('ad_astra:steel_rods', 'ad_astra:steel_rod')
+
+  event.remove('c:storage_blocks/steel', 'ad_astra:steel_block')
+  event.remove('ad_astra:steel_blocks', 'ad_astra:steel_block')
+  event.remove('minecraft:beacon_base_blocks', 'ad_astra:steel_block')
 })
