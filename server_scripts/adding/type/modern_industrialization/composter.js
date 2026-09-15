e66089e126b164790127867c8e16180b74f0ac48
LorpOOO <150320219+LorpOOO@users.noreply.github.com>
2026-09-15 07:15:25 +0300

    Update composter.js

--- server_scripts/adding/type/modern_industrialization/composter.js
+++ server_scripts/adding/type/modern_industrialization/composter.js
@@ -1,12 +1,22 @@
 
 ServerEvents.recipes(event => {
-event.recipes.extended_industrialization.composter(4, 200)
-.itemIn("2x roll_mod:latex_dandelion_stem")
-.itemOut("3x roll_mod:raw_latex")
-.itemOut("1x roll_mod:raw_latex", 0.5)
+  event.recipes.extended_industrialization.composter(4, 200)
+  .itemIn("2x roll_mod:latex_dandelion_stem")
+  .itemOut("3x roll_mod:raw_latex")
+  .itemOut("1x roll_mod:raw_latex", 0.5)
+  
+  event.recipes.extended_industrialization.composter(4, 200)
+  .itemIn("4x roll_mod:latex_dandelion_flower")
+  .itemOut("3x roll_mod:raw_latex")
+  .itemOut("1x minecraft:yellow_dye", 0.5)
 
-event.recipes.extended_industrialization.composter(4, 200)
-.itemIn("4x roll_mod:latex_dandelion_flower")
-.itemOut("3x roll_mod:raw_latex")
-.itemOut("1x minecraft:yellow_dye", 0.5)
+  event.recipes.extended_industrialization.composter(4, 100)
+  .itemIn("8x roll_mod:biomass")
+  .itemIn("1x minecraft:soul_fire_base_blocks")
+  .itemOut("1x mynethersdelight:letios_compost")
+
+  event.recipes.extended_industrialization.composter(4, 100)
+  .itemIn("8x roll_mod:biomass")
+  .itemIn("1x minecraft:dirt")
+  .itemOut("1x farmersdelight:organic_compost")
 })
