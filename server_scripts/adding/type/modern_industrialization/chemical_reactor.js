85e1618138f7c95e7ffa6994c60d51dcf70f2af2
LorpOOO <150320219+LorpOOO@users.noreply.github.com>
2026-09-19 09:19:15 +0300

    Update chemical_reactor.js

--- server_scripts/adding/type/modern_industrialization/chemical_reactor.js
+++ server_scripts/adding/type/modern_industrialization/chemical_reactor.js
@@ -2,9 +2,9 @@ ServerEvents.recipes(event => {
 
 event.recipes.modern_industrialization.chemical_reactor(32, 200)
 .itemIn("2x roll_mod:lithium_dust")
-.fluidIn("modern_industrialization:sulfuric_acid", 1000)
+.fluidIn("modern_industrialization:sulfuric_acid", 250)
 .itemOut("1x roll_mod:lithium_sulfate_dust")
-.fluidOut("modern_industrialization:hydrogen", 1000)
+.fluidOut("modern_industrialization:hydrogen", 250)
     
 event.recipes.modern_industrialization.chemical_reactor(32, 200)
 .itemIn("1x roll_mod:unactivated_zinc_sulfide_powder")
