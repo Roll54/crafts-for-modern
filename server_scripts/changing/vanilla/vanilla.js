803ffce25b6cb6de2c9249bbe29275a9153668f7
LorpOOO <150320219+LorpOOO@users.noreply.github.com>
2026-09-19 09:14:55 +0300

    Update vanilla.js

--- server_scripts/changing/vanilla/vanilla.js
+++ server_scripts/changing/vanilla/vanilla.js
@@ -60,4 +60,40 @@ event.replaceInput(
   'roll_mod:diamond_alloy_plate'
 )
 
+event.replaceInput(
+  { output: 'cosmeticarmoursmod:true_diamond_crown_helmet' },
+  'minecraft:diamond',
+  'roll_mod:diamond_alloy_plate'
+)
+
+event.replaceInput(
+  { output: 'cosmeticarmoursmod:diamond_tiara_helmet' },
+  'minecraft:diamond',
+  'roll_mod:diamond_alloy_plate'
+)
+
+event.replaceInput(
+  { output: 'cosmeticarmoursmod:diamond_halo_helmet' },
+  'cosmeticarmoursmod:diamond_shard',
+  'roll_mod:diamond_alloy_ingot'
+)
+
+event.replaceInput(
+  { output: 'tide:diamond_fishing_rod' },
+  'minecraft:diamond',
+  'roll_mod:diamond_alloy_plate'
+)
+
+event.replaceInput(
+  { output: 'farmersdelight:diamond_knife' },
+  'minecraft:diamond',
+  'roll_mod:diamond_alloy_plate'
+)
+
+event.replaceInput(
+  { output: 'modern_industrialization:diamond_hammer' },
+  'modern_industrialization:diamond_large_plate',
+  'roll_mod:diamond_alloy_large_plate'
+)
+
 })
